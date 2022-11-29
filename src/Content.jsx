1904c0f345195028f4911486d1c583b9d31916bf
Anhelina Kruptseva <109616834+Axigy@users.noreply.github.com>
2022-11-29 18:02:03 +0100

    create files

---
+++ src/Content.jsx
@@ -0,0 +1,9 @@
+const Content = () => {
+  return (
+    <main>
+      <h2>from content</h2>
+    </main>
+  );
+};
+
+export default Content;
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>amd from footer</p>
+    </footer>
+  );
+};
+
+export default Footer;
